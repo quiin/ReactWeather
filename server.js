30ce6e3850736e2a2c21d48dcc39f035778bfa0c
quiin <carlos.reyna21@gmail.com>
2017-02-18 23:36:59 -0600

    Prep repo for Heroky

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 
 var app = express();
-
+const var PORT = process.env.PORT || 3000;
+app.use((req, res, next) => {
+	if (req.headers['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+})
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-	console.log('Server up at port 3000');
+app.listen(PORT, function () {
+	console.log('Express server is up on PORT: ', PORT);
 });
