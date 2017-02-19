6d8484a82b35c7614e9dd253a9b4cd269f91db93
quiin <carlos.reyna21@gmail.com>
2017-02-19 17:55:26 -0600

    About component styled

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,8 +1,31 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
-    <h3>About</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application built on React for Udemy's <a href="udemy.com/the-complete-react-web-app-developer-course/">The Complete React Web App Developer Course</a>
+      </p>
+      <p>
+        Tools used:
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react/">React</a>
+          </li>
+          <li>
+            <a href="http://foundation.zurb.com/sites/docs/">Foundation</a>
+          </li>
+          <li>
+            <a href="https://github.com/quiin/ReactWeather">Git & GitHub</a>
+          </li>
+          <li>
+            <a href="http://thawing-journey-23021.herokuapp.com">Heroku</a>
+          </li>
+        </ul>
+      </p>
+    </div>
   );
 };
 
